650f4a31ec6d9d10ab0cd1ae3f50ab7a83c09dba
sceptross <sceptross@gmail.com>
2015-10-15 17:04:09 +0100

    Do protocol and link layer

--- TP2/noncanonical.c
+++ TP2/noncanonical.c
@@ -5,63 +5,30 @@
 #include <fcntl.h>
 #include <termios.h>
 #include <stdio.h>
-#include <strings.h>
 #include <stdlib.h>
-#include "utils.h"
+#include <strings.h>
+#include <string.h>
 
-#define BAUDRATE B38400
-#define _POSIX_SOURCE 1 /* POSIX compliant source */
-#define FALSE 0
-#define TRUE 1
+#include "linklayer.h"
 
 volatile int STOP=FALSE;
 
 int main(int argc, char** argv)
-{
+{    
     if ( (argc < 2) || 
-  	     ((strcmp("/dev/ttyS0", argv[1])!=0) && 
-  	      (strcmp("/dev/ttyS4", argv[1])!=0) )) {
-      printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
+         ((strcmp("/dev/ttyS0", argv[1])!=0) && 
+          (strcmp("/dev/ttyS4", argv[1])!=0) )) {   
+      printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS4\n");
       exit(1);
     }
-
-  /*
-    Open serial port device for reading and writing and not as controlling tty
-    because we don't want to get killed if linenoise sends CTRL-C.
-  */
-      
+    
     int fd = open(argv[1], O_RDWR | O_NOCTTY );
     if (fd <0) {perror(argv[1]); exit(-1); }
 
-    struct termios oldtio,newtio;
-    tcgetattr(fd,&oldtio); /* save current port settings */
+    llopen(fd,RECEIVER);
 
-    bzero(&newtio, sizeof(newtio));
-    newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
-    newtio.c_iflag = IGNPAR;
-    newtio.c_oflag = 0;
+    llclose(fd);
 
-    /* set input mode (non-canonical, no echo,...) */
-    newtio.c_lflag = 0;
 
-    newtio.c_cc[VTIME]    = 0;   /* inter-character timer unused */
-    newtio.c_cc[VMIN]     = 5;   /* blocking read until 5 chars received */
-
-    tcflush(fd, TCIFLUSH);
-    tcsetattr(fd,TCSANOW,&newtio);
-
-    char message[SERIAL_SU_STRING_SIZE + 1];
-
-    ua_string[0] = SERIAL_FLAG;
-    ua_string[1] = SERIAL_A_ANS_RECEIVER;
-    ua_string[2] = SERIAl_C_UA;
-    ua_string[3] = ua_string[1] ^ ua_string[2];
-    ua_string[4] = SERIAL_FLAG;    
-    
-    
-    sleep(5);
-
-    tcsetattr(fd,TCSANOW,&oldtio);
-    close(fd);
     return 0;
 }
--- TP2/writenoncanonical.c
+++ TP2/writenoncanonical.c
@@ -9,88 +9,26 @@
 #include <strings.h>
 #include <string.h>
 
-#include "utils.h"
-
-#define BAUDRATE B38400
-#define MODEMDEVICE "/dev/ttyS1"
-#define _POSIX_SOURCE 1 /* POSIX compliant source */
-
-#define FALSE 0
-#define TRUE 1
-
-#define MAX_SIZE 255
+#include "linklayer.h"
 
 volatile int STOP=FALSE;
 
 int main(int argc, char** argv)
-{
-    int fd,c, res;
-    struct termios oldtio,newtio;
-    int i, sum = 0, speed = 0;
-
-    char set_string[SERIAL_SU_STRING_SIZE];
-    char ua_string[SERIAL_SU_STRING_SIZE];
-    
+{    
     if ( (argc < 2) || 
   	     ((strcmp("/dev/ttyS0", argv[1])!=0) && 
   	      (strcmp("/dev/ttyS4", argv[1])!=0) )) {	
       printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS4\n");
       exit(1);
     }
-
-	/* Ask message to send */
-	printf("Insert message to send: ");
-	gets(buf);
-
-	/* Open the serial port for sending the message */
-    fd = open(argv[1], O_RDWR | O_NOCTTY );
+    
+    int fd = open(argv[1], O_RDWR | O_NOCTTY );
     if (fd <0) {perror(argv[1]); exit(-1); }
 
-    if ( tcgetattr(fd,&oldtio) == -1) { /* save current port settings */
-      perror("tcgetattr");
-      exit(-1);
-    }
+    llopen(fd,TRANSMITTER);
 
-    bzero(&newtio, sizeof(newtio));
-
-    /* 	Control, input, output flags */
-    newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD; /*  */
-    newtio.c_iflag = IGNPAR;
-    newtio.c_oflag = OPOST;
-
-    /* set input mode (non-canonical, no echo,...) */
-    newtio.c_lflag = 0;
-
-    newtio.c_cc[VTIME]    = 0;   /* inter-character timer unused */
-    newtio.c_cc[VMIN]     = 5;   /* blocking read until 5 chars received */
+    llclose(fd);
 
 
-    tcflush(fd, TCIFLUSH);
-
-    if ( tcsetattr(fd,TCSANOW,&newtio) == -1) {
-      perror("tcsetattr");
-      exit(-1);
-    }
-
-    /* Set SET string values */
-
-    set_string[0] = SERIAL_FLAG;
-    set_string[1] = SERIAL_A_COM_TRANSMITTER;
-    set_string[2] = SERIAL_C_SET;
-    set_string[3] = set_string[1] ^ set_string[2];
-    set_string[4] = SERIAL_FLAG;
-
-    /* Send SET string */
-    res = write(fd,set_string,SERIAL_SU_STRING_SIZE);
-
-    /* */
-
-    sleep(5);
-
-    if ( tcsetattr(fd,TCSANOW,&oldtio) == -1) {
-      perror("tcsetattr");
-      exit(-1);
-    }
-    close(fd);
     return 0;
 }
