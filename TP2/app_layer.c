4f3a22ef82f1974f6264135babe8e45d8fc2ee62
sceptross <afonso.castro11@gmail.com>
2015-10-27 02:09:56 +0000

    Do main

--- TP2/app_layer.c
+++ TP2/app_layer.c
@@ -6,7 +6,9 @@
 #include <unistd.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <termios.h>
 
+#define BAUDRATE B38400
 
 typedef struct {
 	int fd;
@@ -32,13 +34,33 @@ int get_file_info(File_info_t* file_info, char* filePath){
 	return 0;
 }
 
+/*
+	./nserial /dev/ttyS0 ./file.txt baudrate max_tries timeout max_frame_size
+*/
+
 int main(int argc, char** argv){
+
+	if(argc != 6){
+		printf("Usage: %s <port> <filepath> <baudrate> <max_tries> <timeout> <max_frame_size>\n", argv[0]);
+		return 1;
+	}
 
 	int flag = TRANSMITTER;
 	File_info_t file_info;
-	char* filePath = argv[1];
+	int port;
+	int baudrate = BAUDRATE;
+	int max_tries = 3;
+	int timeout = 3;
+	int max_frame_size = 255;
+	char* filePath = argv[2];
 
-	//Linklayer link_layer = llinit();
+	sscanf(argv[1],"/dev/ttyS%d",&port);
+	sscanf(argv[4], "%d", &max_tries);
+	sscanf(argv[5], "%d", &timeout);
+	sscanf(argv[6], "%d", &max_frame_size);
+
+
+	Linklayer link_layer = llinit(port, flag, max_tries, timeout, max_frame_size);
 
 	if (flag == TRANSMITTER){
 		if(get_file_info(&file_info, filePath) == -1)
@@ -46,7 +68,9 @@ int main(int argc, char** argv){
 		printf("Fd: %d, Name: %s, Size:%ld\n", file_info.fd, file_info.name, file_info.size);
 	}
 
-	//lldelete(link_layer);
+
+
+	lldelete(link_layer);
 
 	return 0;
 }
