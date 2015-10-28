cef8a1799982447b7f5bccef44d06815e8682639
sceptross <afonso.castro11@gmail.com>
2015-10-28 22:31:17 +0000

    Neon

--- TP2/app_layer.c
+++ TP2/app_layer.c
@@ -312,6 +312,10 @@ int app_receiver(int argc, char **argv) {
 	int arg;
 	int changeMask[] = {FALSE, FALSE, FALSE, FALSE};
 	for(arg = 3; arg < argc; ++arg){
+		if((arg +1) == argc) {
+			printf("Error while parsing flag %s \n", argv[arg]);
+			return 1;
+		}
 		if(strcmp(argv[arg],"-b") == 0){
 			if(changeMask[0] == FALSE){
 				changeMask[0] = TRUE;
